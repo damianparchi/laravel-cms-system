17a589ad83766ff19a9414a39cf708cb558e4b3d
damianparchi <damian.parchimowicz99@gmail.com>
2023-05-01 19:10:39 +0200

    creating posts from admin panel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,13 +1,39 @@
 <?php
-
 namespace App\Http\Controllers;
+
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
+
 
 class PostController extends Controller
 {
     public function show(Post $post) {
         return view('blog-post', compact('post'));
+    }
+    public function create() {
+        return view ('admin.posts.create');
+    }
+    public function store() {
+
+        $inputs = request() -> validate([
+            'title' => 'required | min:6 | max:255',
+            'post_image' => 'file:image',
+            'body' => 'required',
+        ]);
+
+        if(request()->hasFile('post_image')){
+            $path = request()->file('post_image')->store('public/images');
+
+            Artisan::call('storage:link');
+            $filename = basename(Storage::url($path));
+            $inputs['post_image'] = $filename;
+        }
+
+
+        auth()->user()->posts()->create($inputs);
+        return back();
     }
 }
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,46 @@
+<x-admin-component>
+    @section('content')
+
+
+        <h1>Create</h1>
+
+
+        <form action="{{route('post.store')}}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div style="color: red">{{$errors -> first('title')}}</div>
+            <div style="color: red">{{$errors -> first('post_image')}}</div>
+            <div style="color: red">{{$errors -> first('body')}}</div>
+            <div class="form-group">
+
+
+                <label for="title">Title</label>
+                <input type="text"
+                       name="title"
+                       id="title"
+                       class="form-control"
+                       placeholder="Enter title...">
+            </div>
+            <div class="form-group">
+                <label for="file">File</label>
+                <input type="file"
+                       name="post_image"
+                       id="post_image"
+                       class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="body">Body</label>
+                <textarea name="body"
+                          class="form-control"
+                          id="body"
+                          cols="30"
+                          rows="10"></textarea>
+            </div>
+
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+
+        </form>
+
+    @endsection
+</x-admin-component>
