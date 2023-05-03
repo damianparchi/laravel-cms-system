d85e38487b7bb6e8c656323fa5b0ddec0730fca3
damianparchi <damian.parchimowicz99@gmail.com>
2023-05-03 16:56:33 +0200

    role & permission controllers and models added, create relations for permission, role and user models

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use function Symfony\Component\Translation\t;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,13 @@ class User extends Authenticatable
 
     public function posts() {
         return $this -> hasMany(Post::class);
+    }
+
+    public function roles() {
+        return $this -> belongsToMany(Role::class);
+    }
+
+    public function permissions() {
+        return $this -> belongsToMany(Permission::class);
     }
 }
